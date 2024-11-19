61aa1c71399053539e5d44a11e5cd54f0c9ae1bd
sandeep <venkatas@geekyants.com>
2024-11-19 19:07:08 +0530

    Added post install script to display info

---
+++ scripts/postinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "
+🌍 Thank you for installing localizer-ai! 
+
+Available commands:
+  help            : Show help
+  create-config   : Create a new configuration file
+  translate       : Translate locale files
+
+Get started by running: localizer-ai help
+
+Know more at:
+
+GitHub: https://github.com/TakasiVenkataSandeep-08/localizer-AI
+
+NPM: https://www.npmjs.com/package/localizer-ai
+"
+
+chmod +x ./index.js 
